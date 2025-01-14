8b6c9964e4d2d4c655197c5e4bb3c31c1ef9e321
이대현 <=eddy152264@gmail.com>
2025-01-14 16:48:44 +0900

    2025-01-14

---
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,24 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "responseHtmlServlet", urlPatterns = "/response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        response.getWriter().println("<html>");
+        response.getWriter().println("<body>");
+        response.getWriter().println("<h1>Hello World!</h1>");
+        response.getWriter().println("</body>");
+        response.getWriter().println("</html>");
+    }
+}
